302ed4e3c7fe2552ea10ac7842f3b9069d616e8b
JeffersonS69 <ibarra.jefferson1@gmail.com>
2025-01-06 12:08:26 -0500

    first commit

---
+++ src/payments/payments.service.ts
@@ -0,0 +1,68 @@
+import { Injectable } from '@nestjs/common';
+import { Request, Response } from 'express';
+import { envs } from 'src/config';
+import Stripe from 'stripe';
+import { PaymentSessionDto } from './dto/payment-session.dto';
+
+@Injectable()
+export class PaymentsService {
+  private readonly stripe = new Stripe(envs.stripeSecret);
+
+  async createPaymentSession(paymentSessionDto: PaymentSessionDto) {
+    const { currency, items, orderId } = paymentSessionDto;
+
+    const lineItems = items.map((item) => {
+      return {
+        price_data: {
+          currency: currency,
+          product_data: {
+            name: item.name,
+          },
+          unit_amount: Math.round(item.price * 100),
+        },
+        quantity: item.quantity,
+      };
+    });
+
+    const session = await this.stripe.checkout.sessions.create({
+      payment_intent_data: {
+        metadata: {
+          orderId: orderId,
+        },
+      },
+      line_items: lineItems,
+      mode: 'payment',
+      success_url: envs.stripeSuccessUrl,
+      cancel_url: envs.stripeCancelUrl,
+    });
+
+    return session;
+  }
+
+  async stripeWebhook(req: Request, res: Response) {
+    const sig = req.headers['stripe-signature'];
+    let event: Stripe.Event;
+    const endpointSecret = envs.stripeEndpointSecret;
+
+    try {
+      event = this.stripe.webhooks.constructEvent(
+        req['rawBody'],
+        sig,
+        endpointSecret,
+      );
+    } catch (error) {
+      res.status(400).send(`Webhook Error: ${error.message}`);
+      return;
+    }
+
+    switch (event.type) {
+      case 'charge.succeeded':
+        console.log('Payment was successful');
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    return res.status(200).json({ sig });
+  }
+}
